e1d00796e3c730cd3688398556f00ffd32c10b74
ShirineB3 <shirobbm3@gmail.com>
2025-12-19 21:34:51 +0100

    Add files via upload

---
+++ Seance-02_bis/src/main.py
@@ -0,0 +1,38 @@
+#coding:utf8
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Source des données : https://www.data.gouv.fr/datasets/election-presidentielle-des-10-et-24-avril-2022-resultats-definitifs-du-1er-tour/
+with open("./data/resultats-elections-presidentielles-2022-1er-tour.csv","r") as fichier:
+    contenu = pd.read_csv(fichier)
+
+# Mettre dans un commentaire le numéro de la question
+# Question 1
+
+
+df = pd.DataFrame(contenu)
+print (df) 
+
+nb_lignes = len(contenu)
+nb_colonnes = len(contenu.columns)
+
+print(nb_lignes)
+print(nb_colonnes)
+
+#Affichage du type des colonnes
+#Les object sont les string
+#les int ce sont les entiers
+#les float ce sont des décimaux
+
+print (df.dtypes)
+
+en_tete_colonne = df.head(0)
+print(en_tete_colonne) 
+
+inscrits = df["Inscrits"]
+print(inscrits) 
+
+typesdecolonnes = ["str", "str", "int", "int", "int", "int", "int", "int", "str", "str", "str", "int", "str", "str", "str", "int", "str", "str", "str", "int", "str", "str", "str", "int", "str", "str", "str", "int", "str", "str", "str", "int", "str", "str", "str", "int", "str", "str", "str", "int", "str", "str", "str", "int", "str", "str", "str", "int", "str", "str", "str", "int", "str", "str", "str", "int"]
+for 
+
